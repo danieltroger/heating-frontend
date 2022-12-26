aa84d9c583ee4adeca9a566226671dab5d1e9f99
Daniel Troger <daniel@natur-kultur.eu>
2022-12-26 16:55:06 +0100

    beginning

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,23 @@
+import "../fake_document";
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
+import { DepictAPIWS } from "@depict-ai/utilishared";
+
+let socket: DepictAPIWS;
 
 export default function Home() {
+  if (
+    !socket &&
+    typeof window !== "undefined" &&
+    !(window as unknown as { is_fake: boolean }).is_fake
+  ) {
+    try {
+      debugger;
+      socket = new DepictAPIWS("http://localhost:9321");
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <main>
       <Title>Hello World</Title>
@@ -12,7 +28,7 @@ export default function Home() {
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
-        to learn how to build SolidStart apps. :)
+        to learn how to build SolidStart apps.
       </p>
     </main>
   );

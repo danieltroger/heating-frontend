a9ab89e46d18ce938e41929bdc7218faf569233c
Daniel Troger <daniel@natur-kultur.eu>
2023-01-07 21:09:54 +0100

    remove non working simulator again (PID controller didn't get feedback based on its output)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,6 @@
 import { A, Title } from "solid-start";
-import { socket } from "~/utilities/socket";
-import { random_string } from "@depict-ai/utilishared";
 
 export default function Home() {
-  let input_el: HTMLInputElement;
   return (
     <main>
       <Title>Heating controls</Title>
@@ -19,22 +16,6 @@ export default function Home() {
           <A href="/gpio">GPIO</A>
         </li>
       </ol>
-      <input
-        type="range"
-        min="15"
-        max="85"
-        value="50"
-        step="0.25"
-        ref={input_el!}
-        style={{ width: "100%" }}
-        onInput={() => {
-          socket?.ensure_sent({
-            id: random_string(),
-            command: "write-simulated-temperature",
-            value: input_el.value,
-          });
-        }}
-      />
     </main>
   );
 }

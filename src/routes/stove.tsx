f3ff4178d2465640bb2f459a3fa0c9a7085551b5
Daniel Troger <daniel@natur-kultur.eu>
2023-01-08 17:07:01 +0100

    remove done todo

--- src/routes/stove.tsx
+++ src/routes/stove.tsx
@@ -4,7 +4,6 @@ import { createMemo, Show } from "solid-js";
 import { GPIOObj } from "~/routes/gpio";
 
 export default function Stove() {
-  // - create page in frontend with status for stove: voltage, stove is on off, pid output
   const [get_voltage] = get_backend_synced_signal<number>("voltage");
   const [get_pid_output] = get_backend_synced_signal<number>("pid_output");
   const [get_gpio] = get_backend_synced_signal<GPIOObj>("gpio");

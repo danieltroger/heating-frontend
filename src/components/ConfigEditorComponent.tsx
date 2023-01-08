c2ca005a396a4b2461d30ec800342989584941c3
Daniel Troger <daniel@natur-kultur.eu>
2023-01-08 17:52:05 +0100

    fix saving

--- src/components/ConfigEditorComponent.tsx
+++ src/components/ConfigEditorComponent.tsx
@@ -1,6 +1,7 @@
-import { createComputed } from "solid-js";
+import { createComputed, getOwner } from "solid-js";
 import "./ConfigEditorComponent.css";
 import { get_backend_synced_signal } from "~/utilities/get_backend_synced_signal";
+import { show_toast_with_message } from "~/utilities/show_toast_with_message";
 
 export default function ConfigEditorComponent() {
   let textarea: HTMLTextAreaElement;
@@ -8,6 +9,7 @@ export default function ConfigEditorComponent() {
   const [get_config, set_config] = get_backend_synced_signal<{
     [key: string]: any;
   }>("config");
+  const owner = getOwner()!;
 
   return (
     <div class="config-editor">
@@ -32,7 +34,20 @@ export default function ConfigEditorComponent() {
           if (!set_config) {
             return;
           }
-          const success = await set_config(textarea.value);
+          let object_contents: { [key: string]: any } | undefined;
+          try {
+            object_contents = JSON.parse(textarea.value);
+          } catch (e) {
+            console.error(e);
+            await show_toast_with_message(
+              owner,
+              () => "Error parsing JSON: " + e
+            );
+          }
+          const success =
+            object_contents === undefined
+              ? false
+              : await set_config(object_contents);
           button.animate(
             [
               { backgroundColor: success ? "green" : "red", offset: 0.2 },

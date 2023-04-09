c8d0ff18bccd415ccc5d37644d5640528b83dbaf
Daniel Troger <daniel@natur-kultur.eu>
2023-04-09 18:15:56 +0200

    revert origin

--- src/utilities/socket.ts
+++ src/utilities/socket.ts
@@ -5,7 +5,7 @@ export let socket: DepictAPIWS | undefined;
 
 if (!socket && is_client) {
   try {
-    const u_o = new URL("ws://192.168.1.100");
+    const u_o = new URL(location.origin);
     u_o.protocol = u_o.protocol === "https:" ? "wss:" : "ws:";
     u_o.port = "9321";
     socket = new DepictAPIWS(u_o.href);
